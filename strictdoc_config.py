e1f090fa569b1ef6c1083217f7a44aa9a20e6b16
Park JongBeum <dev@parkjb.com>
2026-04-24 16:23:31 +0900

    hotfix: ignore binaries

--- strictdoc_config.py
+++ strictdoc_config.py
@@ -4,7 +4,8 @@ from strictdoc.core.project_config import ProjectConfig, ProjectFeature
 def create_config() -> ProjectConfig:
     config = ProjectConfig(
         project_title="ACC Requirements Specification",
-        exclude_doc_paths=["README.md", ".venv"],
+        exclude_doc_paths=["README.md", ".venv", "refs"],
+        exclude_source_paths=["refs", "assets", "발표자료", "*.pdf", "*.pptx", "*.docx", "*.xlsx"],
         project_features=ProjectFeature.all()
     )
     return config
